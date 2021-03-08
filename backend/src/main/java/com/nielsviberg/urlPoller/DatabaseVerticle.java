36d5ae006698889d1e42c7dd8b41ed4c9a2112d2
Niels Viberg <nielsviberg@gmail.com>
2021-03-08 13:19:04 +0100

    Same as previous changes

--- backend/src/main/java/com/nielsviberg/urlPoller/DatabaseVerticle.java
+++ backend/src/main/java/com/nielsviberg/urlPoller/DatabaseVerticle.java
@@ -1,2 +1,176 @@
-package com.nielsviberg.urlPoller;public class DatabaseVerticle {
+package com.nielsviberg.urlPoller;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.templates.SqlTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DatabaseVerticle extends AbstractVerticle {
+
+  public enum ErrorCodes {
+    NO_ACTION_SPECIFIED,
+    BAD_ACTION,
+    DB_ERROR
+  }
+
+  private MySQLPool pool;
+  private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseVerticle.class);
+  private static final String SQL_CREATE_SERVICE_TABLE = "create table if not exists Service (Id integer auto_increment primary key, Url varchar(255), Name varchar(255), Status varchar(255), Created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, LastUpdated TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP)";
+  private static final String SQL_GET_SERVICES = "select * from Service";
+  private static final String SQL_ADD_SERVICE = "INSERT INTO Service (Url, Name, Status, Created, LastUpdated) \n" +
+    "VALUES (#{url},#{name},#{status}, current_timestamp(), current_timestamp());";
+  private static final String SQL_DELETE_SERVICE = "DELETE FROM Service where Id=#{id}";
+  private static final String SQL_UPDATE_SERVICE = "UPDATE Service SET Url=#{url}, Name=#{name} WHERE Id=#{id}";
+
+  public static final String CONFIG_DB_PORT = "db.port";
+  public static final String CONFIG_DB_DEFAULT_DB = "db.default.db";
+  public static final String CONFIG_DB_HOST = "db.host";
+  public static final String CONFIG_DB_MAX_POOL_SIZE = "db.max_pool_size";
+  public static final String CONFIG_DB_QUEUE = "db.queue";
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+
+    // TODO: move user credentials into config
+    MySQLConnectOptions connectOptions = new MySQLConnectOptions()
+      .setPort(config().getInteger(CONFIG_DB_PORT, 3306))
+      .setHost(config().getString(CONFIG_DB_HOST, "localhost"))
+      .setDatabase(config().getString(CONFIG_DB_DEFAULT_DB, "url-poller"))
+      .setUser("root")
+      .setPassword("202$Stockholm");
+
+    // Custom pool options
+    PoolOptions poolOptions = new PoolOptions().setMaxSize(config().getInteger(CONFIG_DB_MAX_POOL_SIZE, 5));
+
+    // Create the pool
+    pool = MySQLPool.pool(vertx, connectOptions, poolOptions);
+
+    // Try to establish connection
+    pool.getConnection(ar -> {
+      if (ar.failed()) {
+        LOGGER.error("Could not open a database connection", ar.cause());
+        startPromise.fail(ar.cause());
+      } else {
+
+        // Create necessary tables
+        pool.query(SQL_CREATE_SERVICE_TABLE).execute(create -> {
+          if (create.failed()) {
+            LOGGER.error("Could not create Service table", create.cause());
+            startPromise.fail(create.cause());
+          } else {
+            vertx.eventBus().consumer(config().getString(CONFIG_DB_QUEUE, "db.queue"), this::onMessage);
+            startPromise.complete();
+          }
+        });
+      }
+    });
+  }
+
+  public void onMessage(Message<JsonObject> message) {
+    if (!message.headers().contains("action")) {
+//      LOGGER.error("No action header specified for message with headers {} and body {}",
+//        message.headers(), message.body().encodePrettily());
+      message.fail(ErrorCodes.NO_ACTION_SPECIFIED.ordinal(), "No action header specified");
+      return;
+    }
+    String action = message.headers().get("action");
+
+    switch (action) {
+      case "get-services":
+        getServices(message);
+        break;
+      case "add-service":
+        addService(message);
+        break;
+      case "remove-service":
+        removeService(message);
+        break;
+      case "update-service":
+        updateService(message);
+        break;
+      default:
+        message.fail(ErrorCodes.BAD_ACTION.ordinal(), "Bad action: " + action);
+    }
+  }
+
+  private void getServices(Message<JsonObject> message){
+    pool.query(SQL_GET_SERVICES).execute(res -> {
+      if(res.failed()){
+        reportQueryError(message, res.cause());
+      }
+      RowSet<Row> result = res.result();
+      //TODO: add typing
+      ArrayList services = new ArrayList();
+      result.forEach(row -> services.add(row.toJson()));
+      JsonObject json = new JsonObject()
+        .put("services", services);
+      message.reply(json);
+    });
+  }
+
+  private void addService(Message<JsonObject> message){
+
+    JsonObject json = message.body();
+
+    // SQL template parameters
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("url", json.getString("url"));
+    parameters.put("name", json.getString("name"));
+
+    SqlTemplate.forQuery(pool, SQL_ADD_SERVICE).execute(parameters).onSuccess(res -> {
+      message.reply(new JsonObject());
+    }).onFailure(res -> {
+      reportQueryError(message, res.getCause());
+    });
+  }
+
+  private void removeService(Message<JsonObject> message){
+
+    JsonObject json = message.body();
+
+    // SQL template parameters
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("id", json.getString("id"));
+
+    SqlTemplate.forQuery(pool, SQL_DELETE_SERVICE).execute(parameters).onSuccess(res -> {
+      message.reply(new JsonObject());
+    }).onFailure(res -> {
+      reportQueryError(message, res.getCause());
+    });
+  }
+
+  private void updateService(Message<JsonObject> message){
+
+    JsonObject json = message.body();
+
+    // SQL template parameters
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("id", json.getString("id"));
+    parameters.put("url", json.getString("url"));
+    parameters.put("name", json.getString("name"));
+
+    SqlTemplate.forQuery(pool, SQL_UPDATE_SERVICE).execute(parameters).onSuccess(res -> {
+      message.reply(new JsonObject());
+    }).onFailure(res -> {
+      reportQueryError(message, res.getCause());
+    });
+  }
+
+  private void reportQueryError(Message<JsonObject> message, Throwable cause) {
+    LOGGER.error("Database query error", cause);
+    message.fail(ErrorCodes.DB_ERROR.ordinal(), cause.getMessage());
+  }
 }
--- backend/src/main/java/com/nielsviberg/urlPoller/HTTPServerVerticle.java
+++ backend/src/main/java/com/nielsviberg/urlPoller/HTTPServerVerticle.java
@@ -1,2 +1,230 @@
-package com.nielsviberg.urlPoller;public class HTTPServerVerticle {
+package com.nielsviberg.urlPoller;
+
+import io.vertx.core.*;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.handler.BodyHandler;
+import org.apache.commons.validator.routines.UrlValidator;
+
+public class HttpServerVerticle extends AbstractVerticle {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(HttpServerVerticle.class);
+
+  public static final String CONFIG_HTTP_SERVER_PORT = "http.server.port";
+  public static final String CONFIG_DB_QUEUE = "db.queue";
+
+  private String DBQueue = "db.queue";
+
+  private WebClient client;
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+
+    DBQueue = config().getString(CONFIG_DB_QUEUE, "db.queue");
+
+    HttpServer server = vertx.createHttpServer();
+    client = WebClient.create(vertx);
+
+    Router router = Router.router(vertx);
+    router.get("/").handler(this::serviceGetHandler);
+    router.post().handler(BodyHandler.create());
+    router.post("/").handler(this::serviceAddHandler);
+    router.delete().handler(BodyHandler.create());
+    router.delete("/").handler(this::serviceDeletionHandler);
+    router.put().handler(BodyHandler.create());
+    router.put("/").handler(this::serviceUpdateHandler);
+
+    int portNumber = config().getInteger(CONFIG_HTTP_SERVER_PORT, 8080);
+    server
+      .requestHandler(router)
+      .listen(portNumber, ar -> {
+        if (ar.succeeded()) {
+          LOGGER.info("HTTP server running on port 8080");
+          startPromise.complete();
+        } else {
+          LOGGER.error("Could not start a HTTP server", ar.cause());
+          startPromise.fail(ar.cause());
+        }
+      });
+  }
+
+  private void serviceGetHandler(RoutingContext context) {
+    DeliveryOptions options = new DeliveryOptions().addHeader("action", "get-services");
+    vertx.eventBus().request(DBQueue, null, options, reply -> {
+      if (reply.failed()) {
+        context.fail(reply.cause());
+        return;
+      }
+      JsonObject body = (JsonObject) reply.result().body();
+      context.response().putHeader("content-type", "application/json");
+      context.response().setStatusCode(200);
+      context.response().end(body.encodePrettily());
+    });
+  }
+
+  private void serviceAddHandler(RoutingContext context) {
+    // Get values
+    JsonObject body = context.getBodyAsJson();
+
+    // body is required, notify user
+    if (body == null) {
+      JsonObject badRequest = new JsonObject();
+      badRequest.put("message", "Nothing was sent to server, service url is required");
+      context.response().setStatusCode(400);
+      context.response().end(badRequest.encodePrettily());
+      return;
+    }
+
+    // Fetch service url and name
+    String serviceUrl = body.getString("service");
+    String name = body.getString("name");
+
+    // url is required, notify user
+    if (serviceUrl == null) {
+      JsonObject badRequest = new JsonObject();
+      badRequest.put("message", "The url for the service is required. Please add the url.");
+      context.response().setStatusCode(400);
+      context.response().end(badRequest.encodePrettily());
+      return;
+    }
+
+    // Validate url format
+    UrlValidator urlValidator = new UrlValidator();
+    if (!urlValidator.isValid(serviceUrl)) {
+      JsonObject badRequest = new JsonObject();
+      badRequest.put("message", "The url for the service is incorrect. Please check the spelling of the url.");
+      context.response().setStatusCode(400);
+      context.response().end(badRequest.encodePrettily());
+      return;
+    }
+
+    getStatus(serviceUrl, res -> {
+      if()
+      res.result();
+    })
+
+    JsonObject json = new JsonObject();
+    json.put("url", serviceUrl);
+    json.put("name", name);
+    // json.put("status", status);
+
+    DeliveryOptions options = new DeliveryOptions().addHeader("action", "add-service");
+    vertx.eventBus().request(DBQueue, json, options, reply -> {
+      if (reply.failed()) {
+        context.fail(reply.cause());
+        return;
+      }
+      context.response().setStatusCode(204);
+      context.response().end();
+    });
+  }
+
+  private void serviceDeletionHandler(RoutingContext context) {
+    // Get values
+    JsonObject body = context.getBodyAsJson();
+
+    // body is required, notify user
+    if (body == null) {
+      JsonObject badRequest = new JsonObject();
+      badRequest.put("message", "Nothing was sent to server, service url is required");
+      context.response().setStatusCode(400);
+      context.response().end(badRequest.encodePrettily());
+      return;
+    }
+
+    // Fetch id
+    String id = body.getString("id");
+
+    // url is required, notify user
+    if (id == null) {
+      JsonObject badRequest = new JsonObject();
+      badRequest.put("message", "The id for the service is required. Please add the id.");
+      context.response().setStatusCode(400);
+      context.response().end(badRequest.encodePrettily());
+      return;
+    }
+
+    JsonObject json = new JsonObject();
+    json.put("id", id);
+
+    DeliveryOptions options = new DeliveryOptions().addHeader("action", "remove-service");
+    vertx.eventBus().request(DBQueue, json, options, reply -> {
+      if (reply.failed()) {
+        context.fail(reply.cause());
+        return;
+      }
+      context.response().setStatusCode(204);
+      context.response().end();
+    });
+  }
+
+  private void serviceUpdateHandler(RoutingContext context) {
+    // Get values
+    JsonObject body = context.getBodyAsJson();
+
+    // body is required, notify user
+    if (body == null) {
+      JsonObject badRequest = new JsonObject();
+      badRequest.put("message", "Nothing was sent to server, service url is required");
+      context.response().setStatusCode(400);
+      context.response().end(badRequest.encodePrettily());
+      return;
+    }
+
+    // Fetch service url and name
+    String id = body.getString("id");
+    String serviceUrl = body.getString("service");
+    String name = body.getString("name");
+
+    if (id == null) {
+      JsonObject badRequest = new JsonObject();
+      badRequest.put("message", "The id for the service is required. Please add the id.");
+      context.response().setStatusCode(400);
+      context.response().end(badRequest.encodePrettily());
+      return;
+    }
+
+    // url is required, notify user
+    if (serviceUrl == null) {
+      JsonObject badRequest = new JsonObject();
+      badRequest.put("message", "The url for the service is required. Please add the url.");
+      context.response().setStatusCode(400);
+      context.response().end(badRequest.encodePrettily());
+      return;
+    }
+
+    // Validate url format
+    UrlValidator urlValidator = new UrlValidator();
+    if (!urlValidator.isValid(serviceUrl)) {
+      JsonObject badRequest = new JsonObject();
+      badRequest.put("message", "The url for the service is incorrect. Please check the spelling of the url.");
+      context.response().setStatusCode(400);
+      context.response().end(badRequest.encodePrettily());
+      return;
+    }
+
+    JsonObject json = new JsonObject();
+    json.put("id", id);
+    json.put("url", serviceUrl);
+    json.put("name", name);
+    // json.put("status", status);
+
+    DeliveryOptions options = new DeliveryOptions().addHeader("action", "update-service");
+    vertx.eventBus().request(DBQueue, json, options, reply -> {
+      if (reply.failed()) {
+        context.fail(reply.cause());
+        return;
+      }
+      context.response().setStatusCode(200);
+      context.response().end();
+    });
+  }
 }
+
